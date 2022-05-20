e0d2562b1230ba6d48235712709e0b4bcbe827fc
Demerville Erwan <demerville@hotmail.fr>
2022-05-21 00:53:51 +0200

    Correction sujet 19 exo 1

--- sujet19/22-NSI-19.py
+++ sujet19/22-NSI-19.py
@@ -1,14 +1,21 @@
 # ===> EXERCICE 1 <===
 
 def multiplication(n1, n2):
-    produit = 0  # On crée une variable qui contiendra le produit, initialisée à 0
-    signe = 1  # On crée une variable pour indiquer le signe de n1, 1 si positif, -1 si négatif, initialement à 1
-    if n1 < 0:  # Si n1 est un nombre négatif
-        signe = -1  # On indique dans la variable signe que le signe de n1 est négatif
-        n1 = -n1  # On transforme n1 en nombre positif
-    for _ in range(n1):  # On fait une boucle de n1 itérations 
-        produit += n2  # Puis on ajoute n2 à produit à chaque itération
-    return signe * produit  # On renvoie produit, en le multipliant par signe pour que le bon signe soit associé
+    produit = 0  # On crée une variable qui contiendra la valeur du produit de n1 par n2, initialisée à 0
+    if n2 >= 0:  # Si n2 est un nombre positif ou 0
+        for _ in range(n2):  # On crée une boucle de n2 itérations
+            produit += n1  # À chaque itération, on ajoute n1 à produit
+    else:  # Sinon (n2 est un nombre négatif)
+        for _ in range(-n2):  # On crée une boucle de -n2 itérations
+            produit += -n1 # À chaque itération, on ajoute -n1 à produit
+    return produit  # On renvoie la valeur de produit finale
+
+def multiplication2(n1, n2):
+    """ En une seule ligne, en utilisant les fonctions sum et abs :
+    sum(liste) : Effectue la somme des éléments d'une liste, par exemple sum([3,3,3]) = 9
+    abs(entier) : Renvoie la valeur absolue d'un nombre entier, par exemple abs(-5) = 5 et abs(5) = 5 """
+    
+    return sum([n1 if n2 >= 0 else -n1 for _ in range(abs(n2))])
 
 # ===> EXERCICE 2 <===
 
